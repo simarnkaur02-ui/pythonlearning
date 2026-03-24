03c3759ce8fec750da8ca67ff8af4328256a51f3
Simran kaur Arora <simmiarora97933@gmail.com>
2026-03-24 11:32:30 +0530

    Add files via upload

---
+++ 6_practitc_set1/Question_3.py
@@ -0,0 +1,6 @@
+name = "Simran"
+age = 24
+height = 160.4
+is_student = True
+
+print("The name of the student is " + name + ". She is " + str(age) + " years old. She is " + str(height) + " cms tall." + "Student: " + str(is_student))
