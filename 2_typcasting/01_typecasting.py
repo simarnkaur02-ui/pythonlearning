d2260aa0bd03cb286df62a16c2fa3f7e543e74d3
Simran kaur Arora <simmiarora97933@gmail.com>
2026-03-24 11:26:24 +0530

    Add files via upload

---
+++ 2_typcasting/01_typecasting.py
@@ -0,0 +1,37 @@
+a = 34
+b = "34"
+d = 229
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+
+c = int(b)
+print(c)
+print(type(c))
+
+
+e = str(d)
+print(e)
+print(type(e))
+
+
+
+# Convert string to integer
+num_str = "10"
+num_int = int(num_str)
+print(num_int)  # Output: 10
+print(type(num_int))
+
+# Convert integer to string
+num = 25
+num_str = str(num)
+print(num_str)  # Output: "25"
+print(type(num_str))
+# Convert float to integer
+pi = 3.14
+pi_int = int(pi)
+print(pi_int)   # Output: 3
+print(type(pi_int))
