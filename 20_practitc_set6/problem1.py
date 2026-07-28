ac265dd4729a2b6519ef6a54fbcb43d16df025f7
Simran kaur Arora <simmiarora97933@gmail.com>
2026-07-28 16:23:56 +0530

    Add files via upload

---
+++ 20_practitc_set6/problem1.py
@@ -0,0 +1,8 @@
+class Car:
+    def drive(self):
+        print("car is moving")
+
+Car1 = Car()
+Car1.drive()
+
+        
---
+++ 20_practitc_set6/problem3.py
@@ -0,0 +1,16 @@
+class Animal:
+    def sound(self):
+        print("Some sound")
+
+
+class Dog:
+    def sound(self):
+        print("Bark!")     
+
+
+a =  Animal()
+a.sound()
+
+
+b = Dog()
+b.sound()
